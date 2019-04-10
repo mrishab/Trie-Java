dfe4eb23ef7d9137065ad3ba9c6737e844d97c5b
Rishab Manocha <rishab.manocha@outlook.com>
2019-04-09 21:25:05 -0700

    Cleaned up the preview/test code. Since, the toString() method of the LinkedList class prints the list items separated by comma, iterating over them is not required. Hence, the code for that is removed. The comment is also updated with the result value having sqare braces on either side of the list

--- src/Test.java
+++ src/Test.java
@@ -20,20 +20,16 @@ public class Test {
 
         // Getting the words that match the given prefix (all of them, in this case)
         for (String prefix : prefixes) {
-            System.out.print(prefix.toUpperCase() + ": ");
             List<String> wordList = root.getWords(prefix);
-            for (String retrievedWord : wordList) {
-                System.out.print(retrievedWord + ", ");
-            }
-            System.out.println();
+            System.out.println(prefix.toUpperCase() + ": " + wordList);
         }
     }
     /***
      * Output:
      *
-     * ANTI: antiaircraft, antiaircraftbody, antiaircraftbodyclimax, antiaircraftbodyclimaxseptic,
-     * AUTO: autobiography, autobiographyfocus, autobiographyfocusmobile, autobiographyfocusmobilepilot,
-     * CIRCUM: circumnavigate, circumnavigatescribe, circumnavigatescribevent,
-     * MACRO: macrocosm, macrocosmeconomics, macrocosmeconomicsstructure,
+     * ANTI: [antiaircraft, antiaircraftbody, antiaircraftbodyclimax, antiaircraftbodyclimaxseptic]
+     * AUTO: [autobiography, autobiographyfocus, autobiographyfocusmobile, autobiographyfocusmobilepilot]
+     * CIRCUM: [circumnavigate, circumnavigatescribe, circumnavigatescribevent]
+     * MACRO: [macrocosm, macrocosmeconomics, macrocosmeconomicsstructure]
      */
 }
